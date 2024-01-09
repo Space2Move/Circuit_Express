199ae70ea6be45f78db45183013e8b923d640130
Space2Move <ted@goessling.com>
2024-01-09 11:59:28 -0600

    Made the lights run in a circle and change direction when button A is pressed.

--- code.py
+++ code.py
@@ -13,11 +13,20 @@ colors = [
     (238, 130, 238),# Violet
     (255, 192, 203),# Pink
     (255, 255, 255),# White
-    (0, 0, 0)       # Black
+    (255, 192, 203),# Pink
 ]
 
+direction = 1  # 1 for forward, -1 for backward
+i = 0  # Start from the first LED
+
 while True:
-    for i in range(10):
-        cp.pixels.fill((0, 0, 0))  # Turn off all LEDs
-        cp.pixels[i] = colors[i]  # Turn on the current LED
-        time.sleep(0.25)  # Wait for quarter second
+    cp.pixels.fill((0, 0, 0))  # Turn off all LEDs
+    cp.pixels[i] = colors[i]  # Turn on the current LED
+    time.sleep(0.1)  # Wait for quarter second
+    if cp.button_a:  # If button A is pressed
+        direction *= -1  # Change direction
+    i += direction  # Move to the next or previous LED
+    if i >= 10:  # If the end is reached
+        i = 0  # Start from the beginning
+    elif i < 0:  # If the beginning is reached
+        i = 9  # Start from the end
